df9591c059a969d06bd39a4562a3c9a4ead1d4af
mdimranarafat <mdimran3067333@gmail.com>
2026-08-24 14:23:17 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -8,12 +8,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBgQQKA3HIRW1DYxTR8S6BfUn3T84InNzo",
+  authDomain: "idex-logistics.firebaseapp.com",
+  projectId: "idex-logistics",
+  storageBucket: "idex-logistics.firebasestorage.app",
+  messagingSenderId: "1035854772632",
+  appId: "1:1035854772632:web:f4880c9aabc279db7d53ed",
+  measurementId: "G-ZPSCX2NZF4"
 };
 
 export const app = initializeApp(firebaseConfig);
